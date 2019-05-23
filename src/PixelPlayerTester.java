6fde49c67b1563506c64252e9316d2667e540469
AIRAEN <rlatmdrb1996@gamil.com>
2019-05-23 15:30:19 +0900

    Actions method complete

--- src/PixelPlayerTester.java
+++ src/PixelPlayerTester.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PixelPlayerTester extends Player {
 //	private int[][] valueMap = new int[PixelTester.SIZE_OF_BOARD][PixelTester.SIZE_OF_BOARD];
@@ -8,32 +9,43 @@ public class PixelPlayerTester extends Player {
 	public Point nextPosition(Point lastPosition) {
 		int x = (int)lastPosition.getX(), y = (int)lastPosition.getY();
 		int player = map[(int)currentPosition.getX()][(int)currentPosition.getY()];	// 현재 플레이어가 1번 돌인지 2번 돌인지 판단
-		Point nextPosition = new Point(5,5);
+		Point nextPosition;
 
-	//	myMap = ArrayCopy(map);		// 현재 맵 복사
+	    //	myMap = ArrayCopy(map);		// 현재 맵 복사
+        // 메인 필요
 
 		nextPosition = new Point(x, y);
 		return nextPosition;
-	}/*
-	private Actions(Point lastPosition) {
+	}
+
+	// 주어진 상태에서 가능한 수들의 집합을 돌려준다.
+	private Point[] Actions(Point lastPosition) {
+        ArrayList<Point> actionsPoint = new ArrayList<Point>();
+        // 다음 돌이 놓을 수 있는(map의 값이 0인) 곳 검색
+        for (int i = 0; i < PixelTester.SIZE_OF_BOARD; i++) {
+            if (map[(int)lastPosition.getX()][i] == 0) {
+                actionsPoint.add(new Point((int)lastPosition.getX(), i));
+            }
+        }
 		for (int i = 0; i < PixelTester.SIZE_OF_BOARD; i++) {
-			if (map[i][y]) {
-
-			}
-			if (map[x][i]) {
-
+			if (map[i][(int)lastPosition.getY()] == 0) {
+                actionsPoint.add(new Point(i,(int)lastPosition.getY()));
 			}
 		}
+        Point[] actions = actionsPoint.toArray(new Point[actionsPoint.size()]);  // arraylist -> array
+		return actions;
 	}
-	private Player AlphaBetaSearch() {
-		Player v = MaxValue();
-	}
-	private int MaxValue() {
+	/* 만들어야 할 메소드들
+	private Player AlphaBetaSearch() { Player v = MaxValue(); }
+	private int MaxValue() { ... }
+	private int MinValue() { ... }
+	private int Result(int[][] map, int player) { return result }
+	private boolean TerminalTest() { ... }
+	private Utility() { ... }
+	*/
 
-	}
-	private int MinValue() {
-
-	}
+	// 2차원 배열 복사 메소드
+    // 사용법: arrayDest = ArrayCopy(arraySrc)
 	private final int[][] ArrayCopy(int[][] src) {
 		if (src == null) return null;
 		int[][] dest = new int[src.length][src[0].length];
@@ -44,5 +56,5 @@ public class PixelPlayerTester extends Player {
 
 		return dest;
 	}
-*/
+
 }
