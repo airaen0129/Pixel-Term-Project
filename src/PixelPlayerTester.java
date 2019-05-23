6c8331f24667a15e8a48be1baf7414fb40427246
AIRAEN <rlatmdrb1996@gamil.com>
2019-05-23 14:07:44 +0900

    알고리즘 작성 시작

--- src/PixelPlayerTester.java
+++ src/PixelPlayerTester.java
@@ -1,23 +1,38 @@
 import java.awt.*;
 
 public class PixelPlayerTester extends Player {
-	private int[][] valueMap = new int[PixelTester.SIZE_OF_BOARD][PixelTester.SIZE_OF_BOARD];
-	private boolean isFirstStap = false;
+//	private int[][] valueMap = new int[PixelTester.SIZE_OF_BOARD][PixelTester.SIZE_OF_BOARD];
+//	private boolean isFirstStap = false;
 
 	PixelPlayerTester(int[][] map) { super(map); }
 	public Point nextPosition(Point lastPosition) {
 		int x = (int)lastPosition.getX(), y = (int)lastPosition.getY();
-		int myNum = map[(int)currentPosition.getX()][(int)currentPosition.getY()];
+		int player = map[(int)currentPosition.getX()][(int)currentPosition.getY()];	// 현재 플레이어가 1번 돌인지 2번 돌인지 판단
 		Point nextPosition;
 
-		if (!isFirstStap) {
-			if ()
-		}
-
-
+	//	myMap = ArrayCopy(map);		// 현재 맵 복사
 
 		nextPosition = new Point(x, y);
 		return nextPosition;
+	}
+	private Actions(Point lastPosition) {
+		for (int i = 0; i < PixelTester.SIZE_OF_BOARD; i++) {
+			if (map[i][y]) {
+
+			}
+			if (map[x][i]) {
+
+			}
+		}
+	}
+	private Player AlphaBetaSearch() {
+		Player v = MaxValue();
+	}
+	private int MaxValue() {
+
+	}
+	private int MinValue() {
+
 	}
 	private final int[][] ArrayCopy(int[][] src) {
 		if (src == null) return null;
